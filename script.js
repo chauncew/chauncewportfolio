879f70c01ff4d1f062b758840b95b642f8b5b6c6
Chaunce W <chauncew1137@gmail.com>
2022-11-08 10:46:43 -0600

    First initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+const tl = gsap.timeline()
+tl
+.from(".crimson", {yPercent: -100})
+.from(".green", {xPercent: 100})
+.from(".blue", {yPercent: -100})
+.from(".teal", {xPercent: -100})
+
+ScrollTrigger.create({
+    animation: tl,
+    trigger: ".container",
+    start: "top top",
+    end: "+=4000",
+    pin: true,
+    scrub: true,
+})
+
+
+
+// Hamburger Menu Animations
+const closeContainer = document.querySelector('.close-button')
+
+closeContainer.addEventListener('click', () => {
+    closeContainer.classList.toggle('active')
+})
